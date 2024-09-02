d64a6ed4660b8c9e5b11c9b1ae55fe0c131f5c61
Andy Ward <andy.ward@xbim.net>
2024-09-02 10:44:44 +0100

    Update

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,16 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Xbim.Common.Configuration;
 using Xbim.Ifc;
 using Xbim.Ifc4.Interfaces;
 using Xbim.IO;
 
-Console.WriteLine("Hello, World!");
 
+
+XbimServices.Current.ConfigureServices(s => s
+    .AddLogging(l => l.AddConsole())
+    .AddXbimToolkit()
+    );
+
+var logger = XbimServices.Current.CreateLogger<IfcStore>();
+var diVersion = new Xbim.Common.XbimAssemblyInfo(typeof(ServiceDescriptor).Assembly);
+logger.LogWarning("Using DI version {assmVersion} - File version {fileVersion}", diVersion.AssemblyVersion, diVersion.FileVersion);
 
 string ifcFilePath = @"C:\Users\AndyWard\Desktop\Demo Models\Dormitory-ARC.ifc";
+
 using (FileStream fileStream = new FileStream(ifcFilePath, FileMode.Open, FileAccess.Read, FileShare.Read))
 {
     var schemaVersion = Xbim.Common.Step21.XbimSchemaVersion.Ifc2X3; //GetSchemaVersion(fileStream);
     IfcStore store = IfcStore.Open(fileStream, StorageType.Ifc, schemaVersion, XbimModelType.MemoryModel);
 
-    Console.WriteLine(store.Instances.OfType<IIfcWall>().Count());
+    logger.LogInformation("Found {cnt} products", store.Instances.OfType<IIfcProduct>().Count());
 }
