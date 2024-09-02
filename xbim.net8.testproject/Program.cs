4dc7faaf5ad0f4be5bab192a30a3be693c55b11d
Andy Ward <andy.ward@xbim.net>
2024-09-02 11:10:04 +0100

    Gets model from cloud

--- xbim.net8.testproject/Program.cs
+++ xbim.net8.testproject/Program.cs
@@ -5,8 +5,6 @@ using Xbim.Ifc;
 using Xbim.Ifc4.Interfaces;
 using Xbim.IO;
 
-
-
 XbimServices.Current.ConfigureServices(s => s
     .AddLogging(l => l.AddConsole())
     .AddXbimToolkit()
@@ -16,12 +14,12 @@ var logger = XbimServices.Current.CreateLogger<IfcStore>();
 var diVersion = new Xbim.Common.XbimAssemblyInfo(typeof(ServiceDescriptor).Assembly);
 logger.LogWarning("Using DI version {assmVersion} - File version {fileVersion}", diVersion.AssemblyVersion, diVersion.FileVersion);
 
-string ifcFilePath = @"C:\Users\AndyWard\Desktop\Demo Models\Dormitory-ARC.ifc";
+string ifcFilePath = "https://raw.githubusercontent.com/xBimTeam/XbimEssentials/master/Tests/TestSourceFiles/SampleHouse4.ifc";
 
-using (FileStream fileStream = new FileStream(ifcFilePath, FileMode.Open, FileAccess.Read, FileShare.Read))
-{
-    var schemaVersion = Xbim.Common.Step21.XbimSchemaVersion.Ifc2X3; //GetSchemaVersion(fileStream);
-    IfcStore store = IfcStore.Open(fileStream, StorageType.Ifc, schemaVersion, XbimModelType.MemoryModel);
+using var client = new HttpClient();
+using var httpResult = await client.GetAsync(ifcFilePath);
+using var ifcStream = await httpResult.Content.ReadAsStreamAsync();
 
-    logger.LogInformation("Found {cnt} products", store.Instances.OfType<IIfcProduct>().Count());
-}
+IfcStore store = IfcStore.Open(ifcStream, StorageType.Ifc, XbimModelType.MemoryModel); // Infers IFC schema
+
+logger.LogInformation("Found {cnt} products", store.Instances.OfType<IIfcProduct>().Count());
